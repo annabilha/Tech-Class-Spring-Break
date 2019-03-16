eec1b1f839d605f8ac2f5a70697c3c56e63c3ee9
Anna Bilha <abilha1@babson.edu>
2019-03-16 13:26:18 -0400

    Class 2 - Sat

---
+++ classSaturday.py
@@ -0,0 +1,9 @@
+# import math
+# r = input("Please enter a number:")
+# r=float(r)
+# volume = (4/3)*math.pi*math.pow(r, 3)
+# print('The volume of a sphere of {} radius is {:.3f}.'.format(r, volume))
+# print(type(r))
+
+# import datetime
+# print(datetime.datetime.now())
