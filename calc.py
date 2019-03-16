127d2111373d7bc7023da70a933a28b2feeb910a
Anna Bilha <abilha1@babson.edu>
2019-03-16 12:05:28 -0400

    Class 0

---
+++ calc.py
@@ -0,0 +1,58 @@
+#
+# n = 100
+# print(n+ 42)
+#
+# a = 'ABC'
+# print(a)
+#
+# a="ABC"
+# b=a
+# a="XYZ"
+#
+# print('a is',a,', b is', b)
+
+# first_name = 'John'
+# # # lastname = 'ben'
+# # # print(first_name, lastname)
+# # # print(lastname*10)
+# # #
+# # # print('hello,',first_name)
+# # # print('hello, {}! You are {} years old!'.format(first_name, 20))
+
+# template = 'hello, {}! You are {} years old!'
+#
+# name1= 'Juan'
+# age1= 7
+# name2= 'Jane'
+# age2= 3
+# #
+# # print(template.format(name1, age1))
+# # print(template.format(name2, age2))
+#
+# # print('Pi equals {:.2f}'.format(3.1415))
+#
+# template = 'hello, {name}! You are {age} years old!'
+# print(template.format(age=age1, name=name2))
+
+# import math
+# print(math.pow(math.pi,5))
+# print(math.pi)
+# print(2**38)
+
+# EXERCISES
+
+# n=42
+# print(60*n+42)
+
+# seconds= 42*60+42
+# print("There are {} seconds".format(seconds))
+#
+# km_in_a_mile=1.61
+# miles=1/1.61*10
+# print('There are {:.2f} miles in 10 kilometers.'.format(miles))
+#
+# run=seconds/miles
+# print('The pace is {:.2f} seconds/mile .'.format(run))
+#
+# runminutes=run/60
+# print('The pace is {:.2f} minutes/mile .'.format(runminutes))
---
+++ hello.py
@@ -0,0 +1,6 @@
+print('hello world!')
+print("I don't","care")
+name = 1+2
+print('hello,', name)
+
+
