9960dc700ec1d396c2a0d2dca739624b42a3521d
Anna Bilha <abilha1@babson.edu>
2019-03-16 15:02:13 -0400

    cont

--- classSaturday.py
+++ classSaturday.py
@@ -6,4 +6,84 @@
 # print(type(r))
 
 # import datetime
+# now=datetime.datetime.now()
+# print(now)
+# print(now.hour, now.minute, now.second)
+#
+# import datetime
 # print(datetime.datetime.now())
+
+# CHAPTER 2
+
+# print(3.14e-2)
+#
+#
+# length_of_number = len(str(2**100000))
+# print(length_of_number)
+#
+
+
+import random
+# print(random.random())    #sends random number
+# print(int(random.random()*100))
+# print(random.randint(2,899))
+#
+# print(random.choice([1, 2, 3, 4, 5]))
+# print(random.choice(["pink", "blue", "white"]))
+
+# lower = int(input("Please put a number for the lower bound:"))
+# upper=int(input("Please put a number for the upper bound:"))
+# print(random.randint(lower,upper))
+
+# print('I\'m \"ok".')  # Use escape character \ for '
+# print('I\'m learning\nPython.')
+# print('\\\n\\')   #\n is for paragraph
+#
+# print('\\\t\\')
+# print(r'\\\t\\')
+# print('I\'m learning\tPython.')      #\t is for a large space
+
+# print('''line1
+# ... line2
+# ... line3''')    #'''...''' for line
+
+# print(3>2)
+# print(2<1)
+#
+# print(3>2 and 3>4)    #and= if there is one false thong it will indicate false
+# print(3>2 and 3>1)
+# print(3>2 or 3>4)    #or= if tbhere is one true thing it will indicate true
+
+# age= int(input("How old are you?"))
+#
+# answer = input('Are you in US?')
+# if answer == 'no':
+#     is_in_US = False
+# if answer == 'yes':
+#     is_in_US = True
+#
+# if age >= 21 or not is_in_US:    #or not True = FALSE (vice-versa)
+#     print('Yes, you can')      #it's looking for the true
+# else:                          #it's lookinf for the false
+#     print('No, You cannot')
+
+
+
+
+# # 1.1
+# a = 3
+# print(a + 2)
+# print(type(a))
+#
+# a = a + 1.0
+# print(a)
+# print(type(a))
+#
+# a = 3
+# print(a == 5.0)
+
+
+# !=  #not equal
+# True is assigned as 1
+# False is assigned as 0
+
