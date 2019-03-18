45145ad42b3182a66838b1a3d83ea86180e61038
Anna Bilha <abilha1@babson.edu>
2019-03-18 13:15:52 -0400

    before lumnch break

---
+++ techclassmonday.py
@@ -0,0 +1,38 @@
+# CREATE A FUNCTION
+
+# def function_name(a):
+#     result=function itself
+#     return result
+# or
+# def function_name(a):
+#    return=function itself
+#
+#
+#  EXAMPLE
+# def square_plus_one(a):
+#     result=a**2+1
+#     return result
+# or
+# def square_plus_one(a):
+#     return a**2+1
+
+# print(square_plus_one(3))  will give 10 as the result
+
+# EXAMPLE 2
+# def ahh(a,b):
+#     return a+b
+#
+# print(ahh("pen", "notebook"))
+
+#
+# def quadratic(a, b, c):
+#     return (-b+((b**2)-(4*a*c))**(1/2))/2*a, (-b-((b**2)-(4*a*c))**(1/2))/2*a
+# or
+
+# import math
+# def quadratic(a, b, c):
+#     x_1 = (-b + math.sqrt(b**2-4*a*c))/(2*a)
+#     x_2 = (-b - math.sqrt(b**2-4*a*c))/(2*a)
+#     return x_1, x_2
+#
+# print (quadratic(1,2,-3))      results are 1 and -3
